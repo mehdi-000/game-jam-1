8d0fd1974f16341f76936839434c054bd827d4e0
Mehdi <mehdipopal@outlook.de>
2026-04-10 13:20:48 +0200

    feat: add enemyspawner

---
+++ Assets/Scripts/Enemyspawner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Enemyspanwner : MonoBehaviour
+{
+    [Header("Spawner Settings")] 
+    public Transform player;
+    public GameObject[] enemyPrefabs;
+    public float spawnInterval = 1f;
+    public float spawnRadiusX = 10f;
+    private float spawnTimer = 0f;
+    public float offsetUp = 1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval)
+        {
+            SpawnEnemy();
+            spawnTimer = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject enemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        float offsetRight = Random.Range(spawnRadiusX * 1.2f, spawnRadiusX * 1.5f);
+        Vector3 spawnPosition = new Vector3(player.position.x + offsetRight, player.position.y + Random.Range(0f, offsetUp), 0f);
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+}
